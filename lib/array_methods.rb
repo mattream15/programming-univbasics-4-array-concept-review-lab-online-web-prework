f2fea77c1895e25e5698bcc6347325f1352f102b
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 19:49:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,9 @@ end
 
 def find_max_value(array)
 find_max_value(array) = ([1,2,1,3,4,3,5,4,3,2,1])
+find_max_value(array).sort
 find_max_value(array).length.times do
-  puts find_max_value(array)
+  puts find_max_value(array).return
 end
 
 def find_min_value(array)
