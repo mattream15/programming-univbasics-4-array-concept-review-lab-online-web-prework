aeab72b1659dc8a54e1cd8af2ea11182a699bb33
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 19:49:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+find_max_value(array) = ([1,2,1,3,4,3,5,4,3,2,1])
+find_max_value(array).length.times do
+  puts find_max_value(array)
 end
 
 def find_min_value(array)
