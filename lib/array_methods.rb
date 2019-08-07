88969c73eb6bcc39978a0971bc0eea2a039a783b
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 19:51:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,5 +11,6 @@ find_max_value(array).length.times do
 end
 
 def find_min_value(array)
-  find_min_value(array)
+  find_min_value(array) = ([10,5,3,7,19,1,3,10])
+  find_min_value(array).sort
 end
