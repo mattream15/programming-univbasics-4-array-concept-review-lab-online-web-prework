ddfcd39c2874cbe666a2e8fedfd0d57a71eaf0de
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 19:55:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+5.times do find_element_index(array, value_to_find)
+  puts find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+find_max_value(array) = ([1,2,1,3,4,3,5,4,3,2,1])
+find_max_value(array).sort
+find_max_value(array).length.times do
+  puts find_max_value(array).pop
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value(array) = ([10,5,3,7,19,1,3,10])
+  find_min_value(array).sort
+  find_min_value(array).length.times do
+    puts find_min_value(array).shift
+  end
 end
